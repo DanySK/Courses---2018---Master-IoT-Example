5e65b181babfbe87ea2ba651f5dcb41fd218cb4e
Danilo Pianini <danilo.pianini@unibo.it>
2018-09-26 17:40:14 +0200

    Add Roby's storage test

--- src/main/java/it/unibo/example/StorageActor.java
+++ src/main/java/it/unibo/example/StorageActor.java
@@ -6,27 +6,46 @@ import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.time.Instant;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
-public class StorageActor<T> extends SinkActor<T> {
+public abstract class StorageActor<T> extends SinkActor<T> {
     protected LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
-    Map<Instant,T> storage = new HashMap<>();
+    abstract void store(T t);
+
+    abstract Collection<Map.Entry<Instant,T>> contents();
 
     @Override
     public void preStart() throws Exception {
         super.preStart();
         self().tell(new DoConsume<T>(t -> {
             log.debug("Storing value " + t);
-            storage.put(Instant.now(), t);
+            store(t);
         }), self());
     }
 
     @Override
     protected Receive moreBehaviour(){
         return ReceiveBuilder.create()
-                .match(Messages.Get.class, msg -> sender().tell(storage.entrySet(), self()))
+                .match(Messages.Get.class, msg -> sender().tell(contents(), self()))
                 .build();
+    }
+
+    public static class BasicStorageActor<T> extends StorageActor<T> {
+        Map<Instant,T> storage = new HashMap<>();
+
+
+        @Override
+        void store(T t) {
+            storage.put(Instant.now(), t);
+        }
+
+        @Override
+        Collection<Map.Entry<Instant,T>> contents() {
+            return storage.entrySet();
+        }
     }
 }
--- src/test/java/it/unibo/example/IntegrationTest.java
+++ src/test/java/it/unibo/example/IntegrationTest.java
@@ -3,12 +3,17 @@ package it.unibo.example;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.pattern.PatternsCS;
+import akka.testkit.TestActorRef;
 import akka.testkit.javadsl.TestKit;
 import it.unibo.patterns.ManyToManyBridge;
 import it.unibo.patterns.ObservableAbstractActor;
 import org.junit.*;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Set;
 
 public class IntegrationTest {
     static ActorSystem system;
@@ -24,15 +29,15 @@ public class IntegrationTest {
         system = null;
     }
 
-    ActorRef sensor1, sensor2, bridge, aggregator;
+    ActorRef sensor1, sensor2, bridge, aggregator, thisTest;
 
     @Before
     public void setupActors(){
-        sensor1 = system.actorOf(Props.create(SensorActor.class, () -> new SensorActor<Double>((v) -> 5.00)), "sensor1");
-        sensor2 = system.actorOf(Props.create(SensorActor.class, () -> new SensorActor<Double>((v) -> 10.0)), "sensor2");
-        aggregator = system.actorOf(Props.create(MeanAggregatorActor.class), "aggregator");
+        sensor1 = system.actorOf(Props.create(SensorActor.class, () -> new SensorActor<Double>((v) -> 5.00)));
+        sensor2 = system.actorOf(Props.create(SensorActor.class, () -> new SensorActor<Double>((v) -> 10.0)));
+        aggregator = system.actorOf(Props.create(MeanAggregatorActor.class));
 
-        bridge = system.actorOf(Props.create(ManyToManyBridge.class, () -> new ManyToManyBridge<Object,Object>(d -> new AggregatorActor.SensorData(d))), "bridge");
+        bridge = system.actorOf(Props.create(ManyToManyBridge.class, () -> new ManyToManyBridge<Object,Object>(d -> new AggregatorActor.SensorData(d))));
         bridge.tell(new ManyToManyBridge.Observe(sensor1), ActorRef.noSender());
         bridge.tell(new ManyToManyBridge.Observe(sensor2), ActorRef.noSender());
         bridge.tell(new ManyToManyBridge.ForwardTo(aggregator), ActorRef.noSender());
@@ -64,6 +69,27 @@ public class IntegrationTest {
 
         // Assert
         probe.expectNoMessage();
+    }
+
+    @Test
+    public void testStorage() throws InterruptedException {
+        // Arrange
+        TestActorRef<StorageActor.BasicStorageActor> storage = TestActorRef.create(system,
+              Props.create(StorageActor.BasicStorageActor.class, () -> new StorageActor.BasicStorageActor()));
+        StorageActor.BasicStorageActor object = storage.underlyingActor();
+        TestKit probe = new TestKit(system);
+        aggregator.tell(new ManyToManyBridge.ForwardTo(storage), ActorRef.noSender());
+        aggregator.tell(new ManyToManyBridge.ForwardTo(probe.getRef()), ActorRef.noSender());
+
+        // Act
+        sensor1.tell(new SensorActor.Start(), ActorRef.noSender());
+        sensor2.tell(new SensorActor.Start(), ActorRef.noSender());
+
+        probe.awaitCond(Duration.ofSeconds(6), probe::msgAvailable);
+        System.out.println(object.storage);
+
+        // Assert
+        Assert.assertTrue(!object.storage.isEmpty());
     }
 
 }
